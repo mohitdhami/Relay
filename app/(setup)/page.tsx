6ceb6b4034ae57d056ebb58356a21238114b3418
mohitdhami <mohitdhamipth24@gmail.com>
2023-10-24 20:20:30 +0530

    crafted initial UI model: customize your server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,10 +1,8 @@
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import { InitialModal } from "@/components/modals/initial-modal";
 
-
-//test--
-import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -23,14 +21,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`)
     }
 
-    return (
-        <div>
-            <div>
-            <UserButton afterSignOutUrl="/"/>
-            </div>
-        <p>Create a Server</p>
-        </div>
-    );
+    return <InitialModal />;
 }
 
 export default SetupPage;
