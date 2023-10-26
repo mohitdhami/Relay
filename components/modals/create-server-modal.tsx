6c2b6a15f494f910434d1d54508418745ff8547b
mohitdhami <mohitdhamipth24@gmail.com>
2023-10-26 15:41:24 +0530

    created server specific sidebar ui modal with dummy server options

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -59,6 +59,7 @@ export const CreateServerModal = () => {
         await axios.post("/api/servers", values);
             form.reset();
             router.refresh();
+            onClose();
        }
        catch(error) {
         console.log(error);
