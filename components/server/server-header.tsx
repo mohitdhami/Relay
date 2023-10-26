66fadb4b88b7eca76f3e1a02328369390c941f93
mohitdhami <mohitdhamipth24@gmail.com>
2023-10-26 19:48:52 +0530

    feat: admin can generate server invite link and others can join server through it

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -11,6 +11,8 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { ChevronDown, UserPlus, Settings, Users, PlusCircle, Trash, LogOut } from "lucide-react";
 
+import { useModal } from "@/hooks/use-modal-store";
+
 interface ServerHeaderProps {
     server: ServerWithMembersWithProfiles;
     role?: MemberRole;
@@ -20,6 +22,8 @@ export const ServerHeader = ({
     server,
     role
 }: ServerHeaderProps) => {
+    const { onOpen } = useModal();
+
     const isAdmin = role === MemberRole.ADMIN;
     const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -41,6 +45,7 @@ export const ServerHeader = ({
             >
                 {isModerator && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("invite", { server })}
                         className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
                     >
                         Invite People
